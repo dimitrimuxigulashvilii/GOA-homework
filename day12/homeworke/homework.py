86e68873c08455a32063c27cac7bf13621079cc5
dimitri <160062601+dimitrimuxigulashvilii@users.noreply.github.com>
2024-04-11 22:04:43 +0400

    Add files via upload

---
+++ day12/homeworke/homework.py
@@ -0,0 +1,5 @@
+paroli="paroli"
+user=input()
+while user !=paroli:
+    user=input()
+print("paroli sworia")    
---
+++ day12/homeworke/homeworke.py
@@ -0,0 +1,3 @@
+user=int(input("enter random number:"))
+for m in range(1, user):
+    print(m)
