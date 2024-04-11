0bfd294bd06f205941a583d980e02005ccb121ee
dimitri <160062601+dimitrimuxigulashvilii@users.noreply.github.com>
2024-04-11 22:06:07 +0400

    Add files via upload

---
+++ day15/homework/homeworke1.py
@@ -0,0 +1,9 @@
+dzima=int(input("sheiyvane sheni skolis niShani:"))
+if dzima == 10:
+    print("chad :)")
+elif dzima <=9:
+    print("your son is norm study")
+elif dzima == 5:
+    print("your son is very bad boy he dont teach :(") 
+elif dzima<=5:
+     print("your son is don't study in the school go to a goa ")
