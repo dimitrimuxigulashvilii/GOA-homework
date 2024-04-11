9422cd66b0edc8633e2ff24df7d8e854f408b2ab
dimitri <160062601+dimitrimuxigulashvilii@users.noreply.github.com>
2024-04-11 22:01:22 +0400

    Add files via upload

---
+++ day5/homeworke/homeworke5.py
@@ -0,0 +1,19 @@
+f="father"
+m="mother"
+s="sister"
+b="brother"
+ss="little sister"
+g="grandma"
+
+print ("my " + f + " is 54 years and his " "5years later is 59" )
+print ("my " + m + " is 49 years and his " "5years later is 54" )
+print("my " + s + " is 27 years and his " "5years later is 32" )
+print("my " + b + " is 28 years and his " "5years later is 34" )
+print("my " + ss + " is 13 years and his " "5years later is 18" )
+print("my " + g + " is 79 years and his " "5years later is 84" )
+
+
+
+
+
+
